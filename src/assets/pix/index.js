185ee7ff4cc46a0663487d2c88f1bc59dca3b43c
Corey Angelus <angelusproductions@gmail.com>
2021-02-02 18:27:35 -0500

    make landing page

--- src/assets/pix/index.js
+++ src/assets/pix/index.js
@@ -2,10 +2,12 @@ import homeBackground from './background.png'
 import logoWhite from './logoWhite.png'
 import visionTriangle from './visionTriangle.png'
 import underlines from './underlines.png'
+import arrowRight from './arrowRight.png'
 
 export {
     homeBackground,
     logoWhite,
     visionTriangle,
-    underlines
+    underlines,
+    arrowRight
 }
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -11,14 +11,19 @@ import { paths, portalPwd } from '../../constants'
 const t = {
     header: 'Welcome to the 5G Smart Community.',
     mission1: 'Civitas',
-    mission2: 'disrupts the traditional telecom model by creating a smart town/municipal system producing hyper-local data precisely designed',
-    mission3: 'for the present and the future needs of your community',
+    mission2: 'disrupts the traditional telecom model',
+    mission3: 'by creating a smart town/municipal system producing hyper-local data',
+    mission4: 'precisely designed',
+    mission5: 'for the present and the future needs of your community.',
     portal: 'Portal Access',
     pwdPrompt: 'Please enter the Civitas Secure password to proceed.',
     submit: 'Submit',
     cancel: 'Cancel',
     warning: 'Incorrect password.',
-    placeholder: 'enter password here'
+    placeholder: 'enter password here',
+    contactText1: 'For more information,', 
+    contactText2: 'click the email below to contact us',
+    contactEmail: 'info@civitasnetworks.com'
 }
 
 const Home = ({ history }) => {
@@ -54,15 +59,15 @@ const Home = ({ history }) => {
                     <header id='homeHeader'>{t.header}</header>
                     <div id='overlayBar' />
                     <img id='logoWhite' src={pix.logoWhite} />
-                    <span 
+                    {/* <span 
                         id='portalAccess'
                         onClick={() => setIsPortalModalOpen(true)}
                     >
                         {t.portal}
-                    </span>
+                    </span> */}
                 </section>
             </Parallax>
-            {isPortalModalOpen && (
+            {/* {isPortalModalOpen && (
                 <>
                     <div id='blackBackground' />
                     <div id='portalModal'>
@@ -81,13 +86,21 @@ const Home = ({ history }) => {
                         </section>
                     </div>
                 </>
-            )}
+            )} */}
 
             <section id='missionStatement'>
                 <div id='missionStatementText'>
-                    <b>{t.mission1}</b> {t.mission2} <span id='community'>{t.mission3}</span>
+                    <b>{t.mission1}</b> {t.mission2}
+                    <br/><br/>{t.mission3}<br/><br/>
+                    {t.mission4} <span id='community'>{t.mission5}</span>
                 </div>
                 <img id='missionStatementUnderlines' src={pix.underlines} />
+                <div id='contactInfo'>
+                    <span id='contactText1'>{t.contactText1}</span>
+                    <span id='contactText2'>{t.contactText2}</span>
+                    <img id='arrowRight' src={pix.arrowRight} />
+                    <a href={`mailto: ${t.contactEmail}`}>{t.contactEmail}</a>
+                </div>
             </section>
             <img id='visionTriangle' src={pix.visionTriangle} />
         </main>
